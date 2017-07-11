cc9aded4ee2c0853da0ae49ddd3ba84ff9be9ba7
yacut <yacut@protonmail.com>
2017-07-11 17:18:16 +0200

    refactor

---
+++ checkDynamodbOperations.js
@@ -0,0 +1,85 @@
+// this module checked local aws setup
+// TODO please remove it after DynamoDB implementation
+//
+// local setup:
+// http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html
+// $ java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb -port 4567
+
+var async = require('async');
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: 'some-id',
+  secretAccessKey: 'some-key',
+  region: 'us-east-1',
+  endpoint: 'http://localhost:4567'
+});
+
+var client = new AWS.DynamoDB();
+var dynamodbDoc = new AWS.DynamoDB.DocumentClient(client);
+
+var queryOperationCount = 100;
+var startedDate = new Date();
+async.series({
+  createTable: function(done) {
+    var params = {
+      TableName: 'MyTable',
+      KeySchema: [
+        {
+          AttributeName: 'id',
+          KeyType: 'HASH'
+        }
+      ],
+      AttributeDefinitions: [
+        {
+          AttributeName: 'id',
+          AttributeType: 'S'
+        }
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5
+      }
+    };
+    client.describeTable({ TableName: params.TableName }, function (err, data) {
+      if (err || (data && data.Table && data.Table.TableStatus === 'ACTIVE')) {
+        return done();
+      }
+      client.createTable(params, done);
+    });
+  },
+  putItem: function(done) {
+    var params = {
+      TableName: 'MyTable',
+      Item: {
+        id: { S: '604275e8-17de-4582-bbc2-1b5eaf3b0a4c' },
+        date: { B: (new Date()).toISOString() }
+      }
+    };
+    client.putItem(params, done);
+  },
+  queryBench: function(done) {
+    async.times(queryOperationCount, function(n, next) {
+      var key = '604275e8-17de-4582-bbc2-1b5eaf3b0a4c';
+      var params = {
+        TableName: 'MyTable',
+        KeyConditionExpression: '#id = :key',
+        ExpressionAttributeNames: {
+          '#id': 'id'
+        },
+        ExpressionAttributeValues: {
+          ':key': key
+        }
+      };
+      dynamodbDoc.query(params, next);
+    }, done);
+  }
+}, function(error) {
+  if(error) {
+    console.error('Local DynamoDB has a problem.', error);
+  } else {
+    console.log('Local DynamoDB is operational.');
+    console.log('Opration count: ' + queryOperationCount);
+    console.log('Time: ' + (new Date() - startedDate) + ' ms.');
+  }
+});
